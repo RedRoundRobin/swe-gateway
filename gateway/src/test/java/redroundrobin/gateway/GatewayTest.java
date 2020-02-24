e2205657dfcb5186cbd5ff54f038ea59ffdafafa
Lorenzo Dei Negri <lorenzodeinegri@gmail.com>
2020-02-24 12:06:48 +0100

    Refactoring dei test

--- gateway/src/test/java/redroundrobin/gateway/GatewayTest.java
+++ gateway/src/test/java/redroundrobin/gateway/GatewayTest.java
@@ -2,8 +2,6 @@ package redroundrobin.gateway;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class GatewayTest {
 
     @Test
--- gateway/src/test/java/redroundrobin/gateway/TraduttoreTest.java
+++ gateway/src/test/java/redroundrobin/gateway/TraduttoreTest.java
@@ -2,61 +2,58 @@ package redroundrobin.gateway;
 
 import org.junit.jupiter.api.Test;
 
-import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TraduttoreTest {
+    private Traduttore bytesInOggetto() {
+        byte[] pacchetto1 = {(byte) 0, (byte) 0, (byte) 0, (byte) 0};  // Dispositivo 0: Aggiunta Dispositivo 0 e Aggiunta Sensore 0
+        byte[] pacchetto2 = {(byte) 1, (byte) 1, (byte) 0, (byte) 0};  // Dispositivo 1: Aggiunta Dispositivo 1 e Aggiunta Sensore 0
+        byte[] pacchetto3 = {(byte) 2, (byte) 2, (byte) 0, (byte) 0};  // Richiesta non valida da non aggiungere
+        byte[] pacchetto4 = {(byte) 0, (byte) -1, (byte) 0, (byte) 2}; // Dispositivo 0: Modifica Sensore 0 con 2
+        byte[] pacchetto5 = {(byte) 3, (byte) 1, (byte) 0, (byte) 0};  // Dispositivo 3: Aggiunta Dispositivo 3 e Aggiunta Sensore 0
+        byte[] pacchetto6 = {(byte) 1, (byte) 1, (byte) 1, (byte) 0};  // Dispositivo 1: Aggiungere Sensore 1
+
+        Traduttore traduttore = new Traduttore();
+
+        traduttore.aggiungiSensore(pacchetto1);
+        traduttore.aggiungiSensore(pacchetto2);
+        traduttore.aggiungiSensore(pacchetto3);
+        traduttore.aggiungiSensore(pacchetto4);
+        traduttore.aggiungiSensore(pacchetto5);
+        traduttore.aggiungiSensore(pacchetto6);
+
+        return traduttore;
+    }
 
     @Test
-    void dispositivoInJSON() throws UnknownHostException {
-        Dispositivo d = new Dispositivo(0);
-        d.ottieniSensori().add(new Sensore(0, 0));
-        d.ottieniSensori().add(new Sensore(1, 1));
-        d.ottieniSensori().add(new Sensore(2, 2));
-        d.ottieniSensori().add(new Sensore(3, 3));
+    public void dispositivoInJSON() {
+        List<Sensore> sensori = new ArrayList<>(Arrays.asList(new Sensore(0, 0), new Sensore(1, 1), new Sensore(2, 2), new Sensore(3, 3)));
+        Dispositivo dispositivo = new Dispositivo(0, sensori);
 
+        Traduttore traduttore = new Traduttore();
 
-        Traduttore t = new Traduttore();
+        traduttore.ottieniDispositivi().add(dispositivo);
+        traduttore.ottieniDispositivi().add(dispositivo);
 
-        t.ottieniDispositivi().add(d);
-        t.ottieniDispositivi().add(d);
-
-        String json = t.ottieniJSON();
-
+        String json = traduttore.ottieniJSON();
         assertEquals("[{\"id\":0,\"sensori\":[{\"id\":0,\"dato\":0},{\"id\":1,\"dato\":1},{\"id\":2,\"dato\":2},{\"id\":3,\"dato\":3}]},{\"id\":0,\"sensori\":[{\"id\":0,\"dato\":0},{\"id\":1,\"dato\":1},{\"id\":2,\"dato\":2},{\"id\":3,\"dato\":3}]}]", json);
     }
 
-    private Traduttore bytesInOggetto() {
-        byte[] b1 = {(byte)0,(byte)0,(byte)0,(byte)0};  // Dispositivo 0: Aggiunta Dispositivo 0 e Aggiunta Sensore 0
-        byte[] b2 = {(byte)1,(byte)1,(byte)0,(byte)0};  // Dispositivo 1: Aggiunta Dispositivo 1 e Aggiunta Sensore 0
-        byte[] b3 = {(byte)2,(byte)2,(byte)0,(byte)0};  // Richiesta non valida da non aggiungere
-        byte[] b4 = {(byte)0,(byte)-1,(byte)0,(byte)2}; // Dispositivo 0: Modifica Sensore 0 con 2
-        byte[] b5 = {(byte)3,(byte)1,(byte)0,(byte)0};  // Dispositivo 3: Aggiunta Dispositivo 3 e Aggiunta Sensore 0
-        byte[] b6 = {(byte)1,(byte)1,(byte)1,(byte)0};  // Dispositivo 1: Aggiungere Sensore 1
-
-        Traduttore t = new Traduttore();
-        t.aggiungiSensore(b1);
-        t.aggiungiSensore(b2);
-        t.aggiungiSensore(b3);
-        t.aggiungiSensore(b4);
-        t.aggiungiSensore(b5);
-        t.aggiungiSensore(b6);
-
-        return t;
+    @Test
+    public void bytesInOggetto1() {
+        Traduttore traduttore = bytesInOggetto();
+        assertEquals(3, traduttore.ottieniDispositivi().size());
     }
 
     @Test
-    void bytesInOggetto1() {
-        Traduttore t = bytesInOggetto();
-        assertEquals(3, t.ottieniDispositivi().size());
-    }
-
-    @Test
-    void bytesInOggetto2() {
-        Traduttore t = bytesInOggetto();
-        assertEquals(1, t.ottieniDispositivi().stream()
-                .filter(d -> d.ottieniId() == 0)
+    public void bytesInOggetto2() {
+        Traduttore traduttore = bytesInOggetto();
+        assertEquals(1, traduttore.ottieniDispositivi().stream()
+                .filter(dispositivo -> dispositivo.ottieniId() == 0)
                 .findFirst()
                 .get()
                 .ottieniSensori()
@@ -65,10 +62,10 @@ class TraduttoreTest {
     }
 
     @Test
-    void bytesInOggetto3() {
-        Traduttore t = this.bytesInOggetto();
-        assertEquals(2, t.ottieniDispositivi().stream()
-                .filter(d -> d.ottieniId() == 1)
+    public void bytesInOggetto3() {
+        Traduttore traduttore = bytesInOggetto();
+        assertEquals(2, traduttore.ottieniDispositivi().stream()
+                .filter(dispositivo -> dispositivo.ottieniId() == 1)
                 .findFirst()
                 .get()
                 .ottieniSensori()
@@ -77,13 +74,13 @@ class TraduttoreTest {
     }
 
     @Test
-    void bytesInOggetto4() {
-        Traduttore t = bytesInOggetto();
-        assertEquals(2, t.ottieniDispositivi().stream()
-                .filter(d -> d.ottieniId() == 0)
+    public void bytesInOggetto4() {
+        Traduttore traduttore = bytesInOggetto();
+        assertEquals(2, traduttore.ottieniDispositivi().stream()
+                .filter(dispositivo -> dispositivo.ottieniId() == 0)
                 .findFirst().get()
                 .ottieniSensori().stream()
-                .filter(s -> s.ottieniId() == 0)
+                .filter(sensore -> sensore.ottieniId() == 0)
                 .findFirst().get()
                 .ottieniDato()
         );
