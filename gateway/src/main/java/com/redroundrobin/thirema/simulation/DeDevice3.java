407d06912e76a6ee0719d4f59b98f0e8981cd3e8
Mariano <marianosciacco@gmail.com>
2020-02-26 16:59:03 +0100

    Aggiunti i simulatori per i 3 dispositivi

--- gateway/src/main/java/com/redroundrobin/thirema/simulation/DeDevice3.java
+++ gateway/src/main/java/com/redroundrobin/thirema/simulation/DeDevice3.java
@@ -7,23 +7,25 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class UsDevice1 {
+public class DeDevice3 {
 
     public static void main(String[] args) {
 
         List<Sensor> sensors1 = new ArrayList<>(Arrays.asList(
-                new Sensor(1, 5),
-                new Sensor(2, 5),
-                new Sensor(3, 120)));
+                new Sensor(1, 30),
+                new Sensor(2, 30),
+                new Sensor(3, 30)));
         Device device1 = new Device(1, sensors1);
 
         List<Sensor> sensors2 = new ArrayList<>(Arrays.asList(
-                new Sensor(1, 10),
+                new Sensor(1, 35),
+                new Sensor(3, 35),
+                new Sensor(3, 35),
                 new Sensor(2, 120)));
         Device device2 = new Device(2, sensors2);
 
         List<Device> devices = new ArrayList<>(Arrays.asList(device1, device2));
-        DeviceSimulator deviceSimulator = new DeviceSimulator(6969, devices);
+        DeviceSimulator deviceSimulator = new DeviceSimulator(6971, devices);
 
         deviceSimulator.start();
     }
--- gateway/src/main/java/com/redroundrobin/thirema/simulation/SgDevice2.java
+++ gateway/src/main/java/com/redroundrobin/thirema/simulation/SgDevice2.java
@@ -7,23 +7,28 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class UsDevice1 {
+public class SgDevice2 {
 
     public static void main(String[] args) {
 
         List<Sensor> sensors1 = new ArrayList<>(Arrays.asList(
-                new Sensor(1, 5),
-                new Sensor(2, 5),
+                new Sensor(1, 15),
+                new Sensor(2, 120),
                 new Sensor(3, 120)));
         Device device1 = new Device(1, sensors1);
 
         List<Sensor> sensors2 = new ArrayList<>(Arrays.asList(
-                new Sensor(1, 10),
-                new Sensor(2, 120)));
+                new Sensor(1, 20),
+                new Sensor(2, 20)));
         Device device2 = new Device(2, sensors2);
 
+        List<Sensor> sensors3 = new ArrayList<>(Arrays.asList(
+                new Sensor(1, 120),
+                new Sensor(2, 120)));
+        Device device3 = new Device(3, sensors3);
+
         List<Device> devices = new ArrayList<>(Arrays.asList(device1, device2));
-        DeviceSimulator deviceSimulator = new DeviceSimulator(6969, devices);
+        DeviceSimulator deviceSimulator = new DeviceSimulator(6970, devices);
 
         deviceSimulator.start();
     }
