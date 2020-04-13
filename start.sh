d34697dd4fb031a295f4b9fcec12e53ae229c028
Mariano <marianosciacco@gmail.com>
2020-04-14 00:40:03 +0200

    Fix gateway per demo

--- start.sh
+++ start.sh
@@ -3,12 +3,12 @@ cd gateway
 
 mvn clean package
 
-mv ./target/gateway-*.jar ../../gateway.jar
-mv ./target/device-*.jar ../../devicesimulator.jar
+mv ./target/gateway*.jar ../../gateway.jar
+mv ./target/device*.jar ../../devicesimulator.jar
 cd ../..
 rm -rf tmp 
 
 java -jar /usr/src/gateway/gateway.jar &
 java -jar /usr/src/gateway/devicesimulator.jar
 
-echo "Gateway started..."
+echo "Gateway started with simulator..."
