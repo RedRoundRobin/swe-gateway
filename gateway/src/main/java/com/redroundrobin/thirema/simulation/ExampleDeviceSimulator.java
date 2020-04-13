0835f51d09d4b208449fb7026adfa09706721a22
Mariano <marianosciacco@gmail.com>
2020-04-13 21:32:41 +0200

    Aggiornamento nome device simulator

--- gateway/src/main/java/com/redroundrobin/thirema/simulation/ExampleDeviceSimulator.java
+++ gateway/src/main/java/com/redroundrobin/thirema/simulation/ExampleDeviceSimulator.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class UsDevice1 {
+public class ExampleDeviceSimulator {
 
   public static void main(String[] args) {
 
